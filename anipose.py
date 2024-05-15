84d5325a13a8114f4398e4bc82d89e6704e3a3a1
Brian Hsu <hsubrian@umich.edu>
2024-05-15 17:31:41 -0400

    continue breaking down function

--- anipose.py
+++ anipose.py
@@ -3,14 +3,14 @@ from aniposelib.boards import CharucoBoard, Checkerboard
 from aniposelib.cameras import Camera, CameraGroup
 from aniposelib.utils import load_pose2d_fnames
 
-vidnames = [['R0530_20240401_13-04-14_calibration-charuco-camA.mp4'],
-            ['R0530_20240401_13-04-14_calibration-charuco-camB.mp4'],
-            ['R0530_20240401_13-04-14_calibration-charuco-camC.mp4'],
-            ['R0530_20240401_13-04-14_calibration-charuco-camD.mp4']]
+videos = [['R0530_20240401_13-04-14_calibration-charuco-camA.mp4'],
+          ['R0530_20240401_13-04-14_calibration-charuco-camB.mp4'],
+          ['R0530_20240401_13-04-14_calibration-charuco-camC.mp4'],
+          ['R0530_20240401_13-04-14_calibration-charuco-camD.mp4']]
 
 cam_names = ['A', 'B', 'C','D']
 
-n_cams = len(vidnames)
+n_cams = len(videos)
 
 board = CharucoBoard(7, 10,
                      square_length=25, # here, in mm but any unit works
@@ -19,7 +19,21 @@ board = CharucoBoard(7, 10,
 
 cgroup = CameraGroup.from_names(cam_names, fisheye=False)
 
-cgroup.calibrate_videos(vidnames, board)
+all_rows = cgroup.get_rows_videos(videos, board, verbose=verbose)
+
+cgroup.set_camera_sizes_videos(videos)
+
+error = cgroup.calibrate_rows(all_rows, board,
+                                    init_intrinsics=init_intrinsics,
+                                    init_extrinsics=init_extrinsics,
+                                    verbose=verbose, **kwargs)
+
+# further break down calibrate rows to only get the imgp thing that I need to check plots
+
+
+
+
+cgroup.calibrate_videos(videos, board)
 cgroup.dump('calibration.toml')
 
 # cgroup = CameraGroup.load('calibration.toml')
@@ -28,6 +42,50 @@ cgroup.dump('calibration.toml')
 # --------------------------------------------------------------------
 
 # start breaking down code
+
+# the imgp should have the necessary points and can recreate it in pyplot??
+# the result should be a calibrated plane without any outliers
+def calibrate_rows(self, all_rows, board,
+                   init_intrinsics=True, init_extrinsics=True, verbose=True,
+                   **kwargs):
+    assert len(all_rows) == len(self.cameras), \
+        "Number of camera detections does not match number of cameras"
+
+    for rows, camera in zip(all_rows, self.cameras):
+        size = camera.get_size()
+
+        assert size is not None, \
+            "Camera with name {} has no specified frame size".format(camera.get_name())
+
+        if init_intrinsics:
+            objp, imgp = board.get_all_calibration_points(rows)
+            mixed = [(o, i) for (o, i) in zip(objp, imgp) if len(o) >= 9]
+            objp, imgp = zip(*mixed)
+            matrix = cv2.initCameraMatrix2D(objp, imgp, tuple(size))
+            camera.set_camera_matrix(matrix.copy())
+            camera.zero_distortions()
+
+    print(self.get_dicts())
+
+    for i, (row, cam) in enumerate(zip(all_rows, self.cameras)):
+        all_rows[i] = board.estimate_pose_rows(cam, row)
+
+    new_rows = [[r for r in rows if r['ids'].size >= 8] for rows in all_rows]
+    merged = merge_rows(new_rows)
+    imgp, extra = extract_points(merged, board, min_cameras=2)
+
+    if init_extrinsics:
+        rtvecs = extract_rtvecs(merged)
+        if verbose:
+            pprint(get_connections(rtvecs, self.get_names()))
+        rvecs, tvecs = get_initial_extrinsics(rtvecs, self.get_names())
+        self.set_rotations(rvecs)
+        self.set_translations(tvecs)
+
+    error = self.bundle_adjust_iter(imgp, extra, verbose=verbose, **kwargs)
+
+    return error
+
 def get_rows_videos(self, videos, board, verbose=True):
     all_rows = []
 
@@ -65,3 +123,29 @@ def calibrate_videos(self, videos, board,
                                 init_extrinsics=init_extrinsics,
                                 verbose=verbose, **kwargs)
     return error, all_rows
+
+#board class. the
+def get_all_calibration_points(self, rows):
+    rows = self.fill_points_rows(rows)
+
+    objpoints = self.get_object_points()
+    objpoints = objpoints.reshape(-1, 3)
+
+    all_obj = []
+    all_img = []
+
+    for row in rows:
+        filled_test = row['filled'].reshape(-1, 2)
+        good = np.all(~np.isnan(filled_test), axis=1)
+        filled_app = row['filled'].reshape(-1, 2)
+        objp = np.copy(objpoints)
+        all_obj.append(np.float32(objp[good]))
+        all_img.append(np.float32(filled_app[good]))
+
+    # all_obj = np.vstack(all_obj)
+    # all_img = np.vstack(all_img)
+
+    # all_obj = np.array(all_obj, dtype='float64')
+    # all_img = np.array(all_img, dtype='float64')
+
+    return all_obj, all_img
